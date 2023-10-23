b80f1df7199ece8ea709defee2403a53768d80e0
wzwy <wzwy@tutanota.com>
2023-10-23 08:46:30 +0800

    docs: initial documentation of the command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/// The command takes in a series of tags and descriptions, plus the task's time interval
+/// and then log the data in both taskwarrior and timewarrior
+///
+/// The main use case for this is to track both task and the time it took to complete it after the
+/// task completes without invoking two separate commands `task` and `timew`.
+
 fn main() {
     println!("Hello, world!");
 }

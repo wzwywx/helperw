cd1aa54690872d9c137233c0cb4d840d1f4dd257
wzwy <wzwy@tutanota.com>
2023-10-25 15:01:56 +0800

    feat: Miscellaneous improvements to the main return, error checking, and some logic for time interval parsing
    1. Change main() signature to return Result
    2. Replace all eprintln! with an Error return
    3. After storing the start and end time, remove the arguments for the time interval from the arguments vector

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::env;
+use std::{
+    env,
+    ops::{Add, Deref, Sub},
+    process::exit,
+};
 
 /// The command takes in a series of tags and descriptions, plus the task's time interval
 /// and then log the data in both taskwarrior and timewarrior
@@ -12,12 +16,12 @@ use std::env;
 /// -p <project name>
 /// -t <tags...>
 
-fn main() {
+fn main() -> Result<(), &'static str> {
     let mut args: Vec<String> = env::args().collect::<Vec<String>>();
     args.remove(0);
 
     // TODO: remove this once complete
-    println!("{:?}", args);
+    println!("initial: {:?}", args);
 
     // Since both `task` and `timew` require a description, a lone string without
     // any options will be used as the description
@@ -39,23 +43,37 @@ fn main() {
     // is expected to be the description.
     if !args.contains(&"-".to_string()) {
         // TODO: colorize this
-        eprintln!("Time interval is mising. Have you added '-' between your two datetimes e.g. 20000102T0000 - 20000102T0100 ?");
+        return Err("Time interval is mising. Have you added '-' between your two datetimes e.g. 20000102T0000 - 20000102T0100 ?");
     } else {
-        // `unwrap`` is safe here because the existence of dash was checked in the preceding `if` block
-
+        // `unwrap` won't panic here because the existence of dash was checked in the preceding `if` block
         let dash_position = args.iter().position(|x| x == &"-".to_string()).unwrap();
+        let before_dash_position = dash_position.checked_sub(1);
+        let after_dash_position = dash_position + 1;
 
-        let before_dash_position = dash_position
-            .checked_sub(1)
-            .and_then(|idx_before| args.get(idx_before));
-        let after_dash_position = args.get(dash_position + 1);
+        let start_time_exists = before_dash_position.and_then(|idx_before| args.get(idx_before));
+        let end_time_exists = args.get(after_dash_position);
 
-        // Make sure an argument exists before and after the dash because a closed interval is assumed i.e. start and end date must always be specified.
+        // Make sure an argument exists before and after the dash since a closed interval is assumed i.e. start and end date must always be specified.
         // TODO: A bit repetitive. Minor fix later
-        if before_dash_position.is_none() {
-            eprintln!("Missing a start date. Make sure to put the date before `-`");
-        } else if after_dash_position.is_none() {
-            eprintln!("Missing an end date. Make sure to put the date after `-`");
+        if start_time_exists.is_none() {
+            // TODO: Alternative to returning error - return an error and customize the error message based on the returned error
+            return Err("Missing a start date. Make sure to put the date before `-`");
+        } else if end_time_exists.is_none() {
+            return Err("Missing an end date. Make sure to put the date after `-`");
         }
+
+        let before_dash_position = before_dash_position.unwrap();
+
+        // `unwrap` won't panic here because dash positions for before and after was checked in the preceding `if` block
+        start_time = args.get(before_dash_position).unwrap().to_string();
+        end_time = args.get(after_dash_position).unwrap().to_string();
+
+        // By removing all the time interval arguments including the dash, it can be deduced that the rest will most likely be the description
+        // and the optional tags and projects
+        args.remove(after_dash_position);
+        args.remove(dash_position);
+        args.remove(before_dash_position);
     }
+    println!("end: {:?}", args);
+    Ok(())
 }
